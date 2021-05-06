d6e685352b83dce469aca19f174941260263112e
Rose <r.d.jeanlouis01@gmail.com>
2021-05-05 22:06:35 -0400

    commit tree 6f96df7fcfb1ad2e224cadfdb959e37319033d04

---
+++ js/challenge.js
@@ -0,0 +1,14 @@
+//1. grab the elements 
+const counterThingy = document.querySelector("#counter")
+const minusButtonThingy = document.querySelector("#minus")
+const plusButtonThingy = document.querySelector("#plus")
+const heartButtonThingy = document.querySelector("#heart")
+const pauseButtonThingy = document.querySelector("#pause")
+
+//2. declare counter
+let counter = 0
+
+//3. adding event listeners to EVERYTHING
+minusButtonThingy.addEventListener("click", function(){
+
+})
